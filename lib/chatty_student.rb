8bc8f9de4f4547895af52d4d3e062b21ea0ccad0
Ryan K Burns <ryankburns@gmail.com>
2020-10-13 17:42:59 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,7 +7,16 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-    puts "Pick me!".times(10)
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
+    puts "Pick me!"
   end
 
 end
